65eb4fa5710bb7c0508e3777ff0cea7db39577c3
Alberto <lugli92@gmail.com>
2013-10-07 18:47:58 +0200

    desc problemz

--- public/js/home.js
+++ public/js/home.js
@@ -5,9 +5,11 @@ $(document).ready(function(e) {
 	$('.prodotti ul li')
 	.on('mouseenter', function() {
 		$(this).find('.image span').fadeIn(time);
+		$(this).find('.desc').show(time);
 	})
 	.on('mouseleave', function() {
 		$(this).find('.image span').fadeOut(time);
+		$(this).find('.desc').hide(time);
 	});
 	
 });
