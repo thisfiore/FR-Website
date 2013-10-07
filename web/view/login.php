7d0ba212e37fe976046b62ea801b97a766078f10
Alberto <lugli92@gmail.com>
2013-10-07 12:20:10 +0200

    form fix

--- web/view/login.php
+++ web/view/login.php
@@ -9,20 +9,21 @@
 </head>
 <body>
 	
+	<img src="" />
+	<div class="spacer"></div>
 	<div class="container">
 		<div class="row">
 			<div class="span4 offset4 well">
-				<legend>Log In</legend>
-	          	<div class="alert alert-error">
+	          	<!-- <div class="alert alert-error">
 	                <a class="close" data-dismiss="alert" href="#">×</a>Incorrect Username or Password!
-	            </div>
+	            </div> -->
 				<form method="POST" action="" accept-charset="UTF-8">
-				<input type="text" id="username" class="span4" name="username" placeholder="Username">
-				<input type="password" id="password" class="span4" name="password" placeholder="Password">
+				<input type="text" id="username" class="input-form" name="username" placeholder="Username">
+				<input type="password" id="password" class="input-form" name="password" placeholder="Password">
 	            <!-- <label class="checkbox">
 	            	<input type="checkbox" name="remember" value="1"> Remember Me
 	            </label> -->
-				<button type="submit" name="submit" class="btn btn-info btn-block">Sign in</button>
+				<button type="submit" name="submit" class="btn btn-danger btn-large pull-right">Entra</button>
 				</form>    
 			</div>
 		</div>
