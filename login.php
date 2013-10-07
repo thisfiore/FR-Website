b4d66bb0cfb8a6bbe3864377a21da865bcb656a0
Alberto <lugli92@gmail.com>
2013-10-07 11:39:35 +0200

    Bootstrap set

--- login.php
+++ login.php
@@ -1,5 +1,19 @@
+<!doctype html>
+<html lang="it">
+<head>
+	<meta charset="UTF-8">
+	<title>Food Republic - Login</title>
+
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	
+	
+
+</body>
+</html>
+
 <?php
 
-/*
- * html grezzo del form del Login
- */
+?>
