513e52ac8db2d1bfdeb4afa1024464a459a5ebd9
Gianluca Ricaldone <gianluca70division.com>
2013-10-07 12:18:28 +0200

    struttura index e url pulito

--- public/index.php
+++ public/index.php
@@ -1,15 +1,52 @@
 <?php
-// require_once 'Bootstrap.php';
 
-// $documentRoot = str_replace("index.php", "", __FILE__);
-// $bootstrap = new Bootstrap($documentRoot);
-// $bootstrap->run();
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+define("PROTOCOL", $protocol);
+define("HOSTNAME", $_SERVER['HTTP_HOST']);
 
-if (isset($_COOKIE['userId']) && !empty($_COOKIE['userId'])) {
-	require_once '../web/view/home.php';
-}
+$self = $_SERVER['REQUEST_URI'];
+
+$ds = $self[strlen($self)-1];
+
+$array_self = explode("/",$self);
+$array_self = array_filter($array_self);
+
+
+// echo $ds;
+// echo "<pre>";
+// print_r($array_self);
+// echo "</pre>";
+// die;
+
+// impostazione URL corretto
+if ($ds != "/") {
+	
+	if ($array_self[1] != 'home' || $array_self[1] != 'login') {
+		header("location:".PROTOCOL.HOSTNAME."/");
+	}
+	else {
+		
+		if (isset($_COOKIE['userId']) && !empty($_COOKIE['userId'])) {
+			require_once '../web/view/home.php';
+		}
+		else {
+			require_once '../web/view/login.php';
+		}
+		
+	}
+	
+} 
 else {
-	require_once '../web/view/login.php';
+	
+	if (isset($_COOKIE['userId']) && !empty($_COOKIE['userId'])) {
+		require_once '../web/view/home.php';
+	}
+	else {
+		require_once '../web/view/login.php';
+	}
+	
 }
+
+
 
 ?>
