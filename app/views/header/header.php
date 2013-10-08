7bf101a5f6c86d01fa6ca36f2db51db2078a0ed2
Alberto <lugli92@gmail.com>
2013-10-08 23:41:38 +0200

    Added logout to header + style for header links

--- app/views/header/header.php
+++ app/views/header/header.php
@@ -6,6 +6,6 @@
 	<div class="span8 user text-right">
 		<strong>Alberto Lugli</strong><br />
 		<div>Gruppo Power <i class="icon-white icon-map-marker"></i> Trentino Alto Adige</div>
-<!-- 		<img class="user-img" style="background:url('/img/users/albi.jpg');" />
- -->	</div>
+<a href="/logout/" target="_self">Logout</a>
+</div>
 </div>
