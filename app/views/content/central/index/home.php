0a1b97ec5b515bb7ac586c800eb55540ecf8970d
Alberto <lugli92@gmail.com>
2013-10-08 12:31:06 +0200

    list spesa done add gruppi

--- app/views/content/central/index/home.php
+++ app/views/content/central/index/home.php
@@ -9,9 +9,9 @@
 						data-placement="bottom"
 						data-trigger="hover"
 						data-content="<?php echo $prodotto['desc']?>">
-						
+
 						<div data-attr-id="<?php echo $prodotto['id_prodotto']?>">
-							<div class="image" style="background:url('../../public/img/apples.jpg');">
+							<div class="image" style="background:url('/img/apples.jpg');">
 								<span>+</span>
 							</div>
 						
@@ -34,45 +34,55 @@
 	</div>
 
 	<!-- SIDE LISTA -->
-	<div class="gruppo">
-		
-	</div>
-	<div class="lista">
-		<img class="pagination-centered" src="" />
-		<ul>
-			<?php 
-			if (isset($lista_spesa) && !empty($lista_spesa)) {
-				foreach ($lista_spesa as $cella_lista) { ?>
-
-					<li class="item<?php echo $cella_lista['id_prodotto']?> row-fluid"
-						data-id_ordine="<?php echo $cella_lista['id_ordine']?>" 
-						data-id_prodotto="<?php echo $cella_lista['id_prodotto']?>">
-
-						<div class="alert alert-success span16">
-						<button type="button" class="close" data-dismiss="alert">&times;</button>
-						<span class="span8"><?php echo $cella_lista['prodotto']['nome_prodotto']?></span>
-							
-								<div class="quantity span3" data-quantita="<?php echo $cella_lista['quantita']?>">
-									<span class="meno">-</span>
-									<span><?php echo $cella_lista['quantita']?></span>
-									<span class="piu">+</span>
-								</div>
-								<span class="partial span3 text-right">
-									<?php echo $cella_lista['prodotto']['prezzo']?> €
-								</span>
-							
-						</div>
-					</li>
-			<?php }
-			}
-			?>
-		</ul>
-		
-
-		<div class="subtotal" data-totale="<?php echo $prezzo_finale?>">
-			<span class="text-left">Totale</span>
-			<span class="pull-right"><?php echo $prezzo_finale?> €</span>
+	<div class="side-bar">
+		<div class="gruppo text-center">
+			<h4>Gruppo Power</h4>
+			<div class="people">
+				faccette persone
+			</div>
+			<div><i class="icon-white icon-map-marker"></i> Trentino Alto Adige</div>
+			<br>
 		</div>
-		<button class="btn btn-success btn-large pull-right"><i class="icon-white icon-ok"></i> Acquista</button>
 
+		<div class="lista">
+			<img class="center" src="/img/shopbag.png" />
+			<h4 class="text-center">La Tua Lista della Spesa</h4>
+			<br>
+			<ul>
+				<?php 
+				if (isset($lista_spesa) && !empty($lista_spesa)) {
+					foreach ($lista_spesa as $cella_lista) { ?>
+
+						<li class="item<?php echo $cella_lista['id_prodotto']?> row-fluid"
+							data-id_ordine="<?php echo $cella_lista['id_ordine']?>" 
+							data-id_prodotto="<?php echo $cella_lista['id_prodotto']?>">
+
+							<div class="alert alert-success span16">
+							<button type="button" class="close" data-dismiss="alert">&times;</button>
+							<span class="span8"><?php echo $cella_lista['prodotto']['nome_prodotto']?></span>
+								
+									<div class="quantity span3" data-quantita="<?php echo $cella_lista['quantita']?>">
+										<span class="meno">-</span>
+										<span><?php echo $cella_lista['quantita']?></span>
+										<span class="piu">+</span>
+									</div>
+									<span class="partial span3 text-right">
+										<?php echo $cella_lista['prodotto']['prezzo']?> €
+									</span>
+								
+							</div>
+						</li>
+				<?php }
+				}
+				?>
+			</ul>
+			
+
+			<div class="subtotal" data-totale="<?php echo $prezzo_finale?>">
+				<span class="text-left">Totale</span>
+				<span class="pull-right"><?php echo $prezzo_finale?> €</span>
+			</div>
+			<button class="btn btn-success btn-large pull-right"><i class="icon-white icon-ok"></i> Acquista</button>
+
+		</div>
 	</div>
