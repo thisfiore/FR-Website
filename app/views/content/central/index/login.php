fed101a1892ad0adfdc3f5e458712d0e4cbe3550
Alberto <lugli92@gmail.com>
2013-10-15 11:10:52 +0200

    login tutorial space

--- app/views/content/central/index/login.php
+++ app/views/content/central/index/login.php
@@ -15,5 +15,6 @@
 	            </label> -->
 				<button type="submit" id="submit" name="submit" class="btn btn-danger btn-large btn-block">Entra</button>
 				</form>    
+				<div class="text-center"><a href="#">Scarica il tutorial in pdf</a></div>
 			</div>
 			
