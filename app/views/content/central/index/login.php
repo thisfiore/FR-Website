06ba778e88fe141507b8eb72f9749a27f7ec03e9
Alberto <lugli92@gmail.com>
2013-10-31 16:50:45 +0100

    Intro - Signup - Login

--- app/views/content/central/index/login.php
+++ app/views/content/central/index/login.php
@@ -1,21 +1,129 @@
-
-	<img class="center" src="/img/logo.png" />
-	<div class="spacer"></div>
-
-
-	<div class="span4 center well">
-          	<!-- <div class="alert alert-error">
-                <a class="close" data-dismiss="alert" href="#">×</a>Incorrect Username or Password!
-            </div> -->
-		<form method="POST" class="login" accept-charset="UTF-8">
-			<input type="text" id="username" class="input-form" name="username" placeholder="Mail">
-			<input type="password" id="password" class="input-form" name="password" placeholder="Password">
-            <!-- <label class="checkbox">
-            	<input type="checkbox" name="remember" value="1"> Remember Me
-            </label> -->
-			<button type="submit" id="submit" name="submit" class="btn btn-danger btn-large btn-block">Entra</button>
-		</form>    
-		<div class="text-center">Scarica il <a href="/info/intro">tutorial</a> oppure <a href="/index/signup">registrati</a></div>
+	<!-- LOGIN FORM -->
+	<div id="login" class="wrapperform">
+		<h3 class="text-center form-title"><button class="btn-mod back"><i class="icon-chevron-left"></i></button>Log In</h3>
+		<form method="POST" class="login-form" accept-charset="UTF-8">
+			<div class="control-group">
+				<input type="email" class="input-block-level tck" id="username" name="username" placeholder="Inserisci la tua mail">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="password" class="input-block-level tck" id="password" name="password" placeholder="Inserisci la tua password">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="squaredFour">
+				<input type="checkbox" value="None" id="squaredFour" name="check" />
+				<label for="squaredFour"></label>
+			</div>
+			<span>Ricordami</span>
+			<span class="pull-right"><a href="#">E se avessi dimenticato la mia password?</a></span>
+		</form>
+		<div class="spacerz"></div>
 	</div>
-	<br><br><br><br>
+
+
+	<!-- SIGN UP FORM -->
+	<div id="signup" class="wrapperform">
+		<h3 class="text-center form-title"><button class="btn-mod back"><i class="icon-chevron-left"></i></button>Registrati</h3>
+		<form method="POST" class="signup-form" accept-charset="UTF-8">
+			<div class="control-group">
+				<input type="email" class="input-block-level tck" id="email" name="username" placeholder="Inserisci la tua mail">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="password" class="input-block-level tck" id="pswd1" name="password" placeholder="Scegli una password">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="password" class="input-block-level tck" id="pswd2" placeholder="Ridigita la tua password">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="text" class="input-block-level tck" name="nome" placeholder="Inserisci il tuo nome">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="text" class="input-block-level tck" name="cognome" placeholder="Inserisci il tuo cognome">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="text" class="input-block-level tck" name="cf" placeholder="Inserisci il tuo codice fiscale">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="text" class="input-block-level tck" name="citta" placeholder="Inserisci la tua citt&agrave;">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="text" class="input-block-level tck" name="via" placeholder="Inserisci la tua via">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="text" class="input-block-level tck" name="civico" placeholder="Inserisci il tuo n° civico">
+				<span class="help-inline hide"></span>
+			</div>
+			<div class="control-group">
+				<input type="email" class="input-block-level tck" id="mail_inviter" name="mail_inviter" placeholder="Immetti la mail dell'utente che ti ha invitato">
+				<span class="help-inline hide"></span>
+			</div>
+		</form>
+		<div class="spacerz"></div>
+	</div>
+	
+	<!-- ARROWS -->
+	<div class="wrapper interaction">
+		<span class="prev"><img src="/img/left-arrow.png"></span>
+		<span class="next"><img src="/img/right-arrow.png"></span>
+	</div>
+
+	<!-- SLIDER -->
+	<div class="wrapper">
+		<div class="content">
+			<div class="slide1">
+				<img src="/img/intro1.png">
+				<div class="spacerz"></div><div class="spacerz"></div>
+				<p class="title">Benvenuto su <strong>Food Republic</strong>!</p>
+				<div class="spacerz"></div>
+				<p>Food Republic è il modo migliore<br/>
+				   per comprare cose <em>buone</em><br/>
+				   ad un prezzo <em>equo</em>.</p>
+				<div class="spacerz"></div>
+			</div>
+			<div class="slide2">
+				<img src="/img/intro2.png">
+				<div class="spacerz"></div><div class="spacerz"></div>
+				<p class="title">I prodotti di <strong>Food Republic</strong></p>
+				<div class="spacerz"></div>
+				<p>sono <em>buoni</em>, <em>sani</em> e ad un prezzo <em>equo</em>.<br>
+  				   Tutti i prodotti sono bio, certificati o auto-certificati.</p>
+  				<p>I nostri prodotti sono così sani<br>
+				   principalmente perchè sono di stagione.</p>
+				<div class="spacerz"></div>
+			</div>
+			<div class="slide3">
+				<img src="/img/intro3.png">
+				<div class="spacerz"></div><div class="spacerz"></div>
+				<p class="title">E non <strong>inquinano</strong>..</p>
+				<div class="spacerz"></div>
+				<p>ottimizza il sistema <em>trasporti</em> riducendo<br>
+				   al minimo i chilometri percorsi dal tuo cibo!</p>
+				<div class="spacerz"></div>
+			</div>
+			<!-- <div class="slide4">
+				Prova4
+			</div> -->
+		</div>
+		<div class="bottom">
+			<div class="pointers">
+				<ul>
+					<li class="active" data-slide="1" data-color="#ef6957" data-sub-color="#ed978e">  
+					</li><li data-slide="2" data-color="#89cee5" data-sub-color="#bbecf9">
+					</li><li data-slide="3" data-color="#a2e288" data-sub-color="#bff9a5">
+					<!-- </li><li data-slide="4" data-color="#ef6957" data-sub-color="#ed978e"> -->
+					</li>
+				</ul>
+			</div>
+			<button class="pull-left signup" data-check="0">Registrati</button>
+			<button class="pull-right login" data-check="0">Entra</button>
+		</div>
+	</div>
 			
--- public/js/login.js
+++ public/js/login.js
@@ -1,33 +1,279 @@
 $(document).ready(function(e) {
-	
-	$('#submit').click(function(event) {
-		event.preventDefault();
-		
-		$('form.login').ajaxForm({
-			url : '/index/login/',
-			type : 'POST',
-			dataType : 'json',
-			beforeSubmit : function() {
-				if ($('#username').val() == '') {
-					$('#username').css({'border':'red 2px solid'});
-					return false;
-				}
-				
-				if ($('#password').val() == '') {
-					$('#password').css({'border':'red 2px solid'});
-					return false;
-				}
-			},
-			success : function(response) {
-				console.log(response);
-				if (response.status == 'OK') {
-					window.location.href = '/index/';
-				}
-				else {
-					alert('Username o Password incorretti');
-				}
-			}
-		}).submit();
+
+	// Azione slide, click su freccine bianche
+	$('.interaction').on('click', 'span', function() {
+		var activeSlide = $('.pointers ul').find('li.active').data('slide');
+		var numberSlides = $('.pointers ul li').size();
+
+		if ( $(this).hasClass('next') ) {
+			if ( activeSlide == numberSlides ) { return false; }
+			var color = $('.pointers ul li.active').removeClass('active').next().addClass('active').data('color');
+			var subColor = $('.pointers ul li.active').data('sub-color');
+			$('.pointers ul li:not(.active)').css('background', subColor);
+			
+			$('.content').css({left: '-'+ activeSlide +'00%'});
+			$('.wrapper').css('background', color);
+		} 
+		if ( $(this).hasClass('prev') ) {
+			if ( activeSlide == 1 ) { return false; }
+			activeSlide = activeSlide - 2;
+			var color = $('.pointers ul li.active').removeClass('active').prev().addClass('active').data('color');
+			var subColor = $('.pointers ul li.active').data('sub-color');
+			$('.pointers ul li:not(.active)').css('background', subColor);
+			
+			$('.content').css({left: '-'+ activeSlide +'00%'});
+			$('.wrapper').css('background', color);
+		}
 	});
+
+	
+	// Controllo su tutti i tck
+	$('.tck').on('focus', function() {
+
+		$(this).parent('.control-group').removeClass('error');
+		$(this).next('span').hide(200);
+		
+		if ($(this).attr('type') == 'password' ) {
+			$("#pswd1").css("border", "");
+			$("#pswd2").css("border", "");
+		}
+		else {
+			$(this).css("border", "");
+		}
+	});
+
+	// Invio form anche con INVIO
+	$('.tck').on('keypress', function(event) {
+		if (event.which == 13) {
+			if ( $(this).closest('form').attr('class') == 'login-form' ) { $( ".login" ).trigger( "click" ); }
+			if ( $(this).closest('form').attr('class') == 'signup-form' ) { $( ".signup" ).trigger( "click" ); } 
+        }
+	});
+
+	// quando schiaccio bottone entra
+	// 1. animazione
+	// 2. invio form
+	$('.login').on('click', function() {
+		var check = $(this).data('check');
+		if ( check == 0 ) {
+			var sliding = $('#login').css('margin-top');
+		
+			$(this).animate({
+				display: 'block',
+				width: '100%'
+			}, 400, function() {
+				$(this).removeClass('pull-right');
+			}).prev().hide(200);
+			$('.wrapper').animate({
+				marginTop: '-251px'
+			}, { duration: 300, queue: false });
+			$('#login').animate({
+				top: 0
+			}, { duration: 400, queue: false });
+
+			$('.wrapper.interaction').fadeOut(200);
+			$('#username').focus();
+			$(this).data('check', 1);
+		} else {
+			event.preventDefault();
+		
+			$('form.login-form').ajaxForm({
+				url : '/index/login/',
+				type : 'POST',
+				dataType : 'json',
+				beforeSubmit : function() {
+					if ($('#username').val() == '') {
+						$("#username").parent('.control-group').addClass('error');
+						$("#username").next('span').text('Inserisci un indirizzo email valido').show(200);
+						return false;
+					}
+					
+					if ($('#password').val() == '') {
+						$("#password").parent('.control-group').addClass('error');
+						$("#password").next('span').text('La password deve avere un valore').show(200);
+						return false;
+					}
+				},
+				success : function(response) {
+					console.log(response);
+					if (response.status == 'OK') {
+						window.location.href = '/index/';
+					}
+					else {
+						alert('Username o Password incorretti');
+					}
+				}
+			}).submit();
+		}
+		
+	});
+
+
+	// bottone indietro da login o registrati
+	$('.back').on('click', function() {
+		if ( $('.login').data('check') == 1 ) {
+			$('.login').data('check', 0);
+			
+			$('.login').animate({
+				display: 'block',
+				width: '120px'
+			}, 400, function() {
+
+			}).addClass('pull-right').prev().show(200);
+			$('.wrapper').animate({
+				marginTop: '0px'
+			}, { duration: 300, queue: false });
+			$('#login').animate({
+				top: '-264px'
+			}, { duration: 400, queue: false });
+
+			$('.wrapper.interaction').fadeIn(200);
+		}
+
+		if ( $('.signup').data('check') == 1 ) {
+			$('.signup').data('check', 0);
+			
+			$('.signup').animate({
+				display: 'block',
+				width: '120px'
+			}, 400, function() {
+				$('.signup').addClass('pull-right');
+			}).next().show(200);
+			$('.wrapper').animate({
+				marginTop: '0px'
+			}, { duration: 300, queue: false });
+			$('#signup').animate({
+				top: '-740px'
+			}, { duration: 400, queue: false });
+
+			$('.wrapper.interaction').fadeIn(200);
+		}
+	});
+
+
+	// click su bottone registrati
+	// 1. animazione
+	// 2.invio form
+	$('.signup').on('click', function() {
+		var check = $(this).data('check');
+		if ( check == 0 ) {
+			var sliding = $('#signup').css('margin-top');
+		
+			$(this).animate({
+				display: 'block',
+				width: '100%'
+			}, 400, function() {
+				$(this).removeClass('pull-right');
+			}).next().hide(200);
+			$('.wrapper').animate({
+				marginTop: '225px'
+			}, { duration: 300, queue: false });
+			$('#signup').animate({
+				top: 0
+			}, { duration: 400, queue: false });
+
+			$('.wrapper.interaction').fadeOut(200);
+			$('#email').focus();
+			$(this).data('check', 1);
+		} else {
+			event.preventDefault();
+			
+			$('form.signup-form').ajaxForm({
+				url : '/index/signup/',
+				type : 'POST',
+				dataType : 'json',
+				beforeSubmit : function() {
+					var check = true;
+					
+	//				Controllo se i campi obbligatori sono inseriti
+					$('.tck').each(function(index) {
+						if($(this).val() == '') { 
+							$(this).parent('.control-group').addClass('error');
+							$(this).next('span').text('Ti sei dimenticato qualcosa qui sopra').show(200);
+							check = false;	
+						}
+					});
+					
+	//				Controllo se l'utente ha inserito una mail valida
+					var email = $("#email").val();
+					var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+					if (!filter.test(email)) {
+						$("#email").parent('.control-group').addClass('error');
+						$("#email").next('span').text('Inserisci un indirizzo email valido').show(200);
+						check = false;
+					}
+					
+	//				Controllo se la password  stata inserita 2 volte correttamente e che sia lunga almeno 6 caratteri
+					var pswd1 = $("#pswd1").val();
+					var pswd2 = $("#pswd2").val();
+					if (pswd1 != pswd2) {
+						$("#pswd1").parent('.control-group').addClass('error');
+						$("#pswd2").parent('.control-group').addClass('error');
+						$("#pswd2").next('span').text('Le password devono essere uguali').show(200);
+						check = false;
+					}
+					if (pswd1.length < 6) {
+						$("#pswd1").parent('.control-group').addClass('error');
+						$("#pswd1").next('span').text('La password deve essere lunga almeno 6 caratteri').show(200);
+						check = false;
+					}
+					if (pswd2.length < 6) {
+						$("#pswd2").parent('.control-group').addClass('error');
+						$("#pswd2").next('span').text('La password deve essere lunga almeno 6 caratteri').show(200);
+						check = false;
+					}
+					
+
+					var mail_inviter = $("#mail_inviter").val();
+					$.ajax({
+						url : '/index/emailInvito/',
+						type : 'GET',
+						dataType : 'json',
+						async: false,
+						data : {
+							mail_inviter : mail_inviter,
+							email : email,
+						},
+						success : function(response) {
+							
+							if (response.status == 'USER') {
+								$("#email").parent('.control-group').addClass('error');
+								$("#email").next('span').text(response.message).show(200);
+								check = false;
+	//							validateResult(check);
+							}
+							else if (response.status == 'ERR') {
+								$("#mail_inviter").parent('.control-group').addClass('error');
+								$("#mail_inviter").next('span').text(response.message).show(200);
+								check = false;
+	//							validateResult(check);
+							}
+							
+						}
+					});
+					
+	//				Se ci sono stati errori blocco tutto
+					if (check == false) {
+						console.log('here');
+						return false;
+					}
+					
+				},
+				success : function(response) {
+					console.log(response);
+					if (response.status == 'OK') {
+						window.location.href = '/index/';
+					}
+					else {
+						alert('Impossibile iscriversi!');
+					}
+				}
+			}).submit();
+
+		}
+
+	});
+
+
 	
 });
