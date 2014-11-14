6d00f575cedc9b7c48804b9eda2122637f3eaf3d
Alberto <lugli92@gmail.com>
2014-11-14 14:45:37 +0100

    fix copy

--- app/views/content/central/index/linkUmbria.php
+++ app/views/content/central/index/linkUmbria.php
@@ -2,9 +2,13 @@
     <img class="center" src="/img/shopbag.png" />
     <h1>Grazie <?php echo $utente['nome']?>!</h1>
     
-	<p>Il tuo prezioso gesto ci sta aiutando a portare Food Republic in Umbria!</p>
-	<p>Aiutaci ancora un poco rispondendo ad alcune domande di un <a href="http://www.typeform.com/" target="_BLANK">questionario</a>. Ci serve per creare un servizio che risponda meglio alle <strong>tue</strong> esigenze.</p>
+	<p>Il tuo prezioso gesto ci sta aiutando a portare i prodotti umbri a casa tua!</p>
+	<p>Aiutaci ancora un poco rispondendo ad alcune domande di un questionario.<br>Ci serve per creare un servizio che risponda meglio alle <strong>tue</strong> esigenze.</p>
 	
+	<br>
+	<a class="login-link" href="http://www.typeform.com/" target="_BLANK" style="text-decoration: none; color: white; line-height: 40px;">Questionario</a>
+	<br>
+
 	<br><br>
 	<p class="pull-right"><em>Il Team di <strong>Food Republic</em></strong></p>
 	<br><br>
