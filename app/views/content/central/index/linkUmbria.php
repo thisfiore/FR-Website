46f505d34e965e4fc3fbf179ad73c32c9d8aede9
Alberto <lugli92@gmail.com>
2014-11-14 17:25:49 +0100

    type form

--- app/views/content/central/index/linkUmbria.php
+++ app/views/content/central/index/linkUmbria.php
@@ -6,10 +6,13 @@
 	<p>Aiutaci ancora un poco rispondendo ad alcune domande di un questionario.<br>Ci serve per creare un servizio che risponda meglio alle <strong>tue</strong> esigenze.</p>
 	
 	<br>
-	<a class="login-link" href="http://www.typeform.com/" target="_BLANK" style="text-decoration: none; color: white; line-height: 40px;">Questionario</a>
+	<a class="login-link typeform-share button" href="https://foodrepublic1.typeform.com/to/IU6uoe" data-mode="1" target="_blank" style="text-decoration: none; color: white; line-height: 40px;">Questionario</a>
 	<br>
+
 
 	<br><br>
 	<p class="pull-right"><em>Il Team di <strong>Food Republic</em></strong></p>
 	<br><br>
  </div>
+
+ <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()</script>
