628600b47cabef40a1a33eed7030448beec78184
Alberto <lugli92@gmail.com>
2014-11-14 17:29:39 +0100

    fix

--- app/views/content/central/index/linkUmbria.php
+++ app/views/content/central/index/linkUmbria.php
@@ -6,7 +6,7 @@
 	<p>Aiutaci ancora un poco rispondendo ad alcune domande di un questionario.<br>Ci serve per creare un servizio che risponda meglio alle <strong>tue</strong> esigenze.</p>
 	
 	<br>
-	<a class="login-link typeform-share button" href="https://foodrepublic1.typeform.com/to/IU6uoe" data-mode="1" target="_blank" style="text-decoration: none; color: white; line-height: 40px;">Questionario</a>
+	<a class="login-link typeform-share" href="https://foodrepublic1.typeform.com/to/IU6uoe" data-mode="1" target="_blank" style="text-decoration: none; color: white; line-height: 40px;">Questionario</a>
 	<br>
 
 
